d009636f091f52c5e1a06cb7a180b2cd108b2c4d
ykent <ppscam225@gmail.com>
2022-02-01 15:46:14 +0900

    ヘッダーのjsを少し書いた。

--- header.js
+++ header.js
@@ -10,25 +10,11 @@ function hamburger() {
   line1.classList.toggle('line_1');
   line2.classList.toggle('line_2');
   line3.classList.toggle('line_3')
-
-  // line1.classList.add('line_1');
-  // line2.classList.add('line_2');
-  // line3.classList.add('line_3');
-
-
-
-
   navi.classList.toggle('in');
 
   document.getElementById('popupCover').classList.toggle('black-out');
-
-
-
-
-
-  // burger.classList.add('change-into-white');
-  // burger.classList.remove('hamburger')
 }
+
 document.getElementById('hamburger').addEventListener('click' , function () {
   hamburger();
 } );
